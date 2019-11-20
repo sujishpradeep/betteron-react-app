4156d8e6c5716b0bee824636c44d14e1d84ef9ab
Sujish Pradeep <sujishp164@gmail.com>
2019-11-20 23:08:57 +1100

    Changed API endpoint

--- src/services/tagService.js
+++ src/services/tagService.js
@@ -2,7 +2,7 @@ import http from "./httpservice";
 // import { apiUrl } from "../config.json";
 import config from "../config";
 
-const apiTags = config.API_URL + "/api/tags/";
+const apiTags = config.API_URL + "/upstacks/tags/";
 
 export function getTags() {
   return http.get(apiTags);
