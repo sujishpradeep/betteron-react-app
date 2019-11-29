2a9262c140ebf152a595e0b00339a5888cc77737
Sujish Pradeep <sujishp164@gmail.com>
2019-11-29 11:24:32 +1100

    Added sitemap.xml

---
+++ src/sitemaps/sitemap-routes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route } from "react-router";
+
+export default (
+  <Route>
+    <Route path="/topics/:topicname" />
+
+    <Route path="/aboutus" />
+
+    <Route path="/" />
+  </Route>
+);
